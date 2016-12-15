87b362d9b3882123d83d6cb9908cacbc71583c6d
John D. Bower <jdbower0525@gmail.com>
2016-12-15 14:31:04 -0500

    working on heroku load

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn myproject.wsgi —-log-file -
+web: gunicorn gradcountdownproj.wsgi —-log-file -
--- countdownapp/urls.py
+++ countdownapp/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from . import views
+from countdownapp import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),

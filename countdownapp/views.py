84be5decb16914864b1ba8962b140255627125fc
John D. Bower <jdbower0525@gmail.com>
2016-12-15 14:23:51 -0500

    setting up heroku

--- countdownapp/views.py
+++ countdownapp/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse("You made it!")
+    return HttpResponse("You will likely be the only one who sees this page...")
